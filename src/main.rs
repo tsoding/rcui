fa65a2a4a2fd0758f7ae4e15804a2fb636342951
rexim <reximkut@gmail.com>
2020-11-03 21:11:20 +0700

    Introduce Horizontal Align for Text widget

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ struct Rect {
 }
 
 enum Event {
+    Realign(HAlign)
 }
 
 trait Widget {
@@ -59,22 +60,60 @@ impl Widget for VBox {
         }
     }
 
-    fn handle_event(&mut self, _event: &Event) {
+    fn handle_event(&mut self, event: &Event) {
+        for widget in self.widgets.iter_mut() {
+            widget.handle_event(event);
+        }
     }
 }
 
+#[derive(Clone, Copy)]
+enum HAlign {
+    Left,
+    Centre,
+    Right,
+}
+
 struct Text {
-    text: String
+    text: String,
+    halign: HAlign,
 }
 
 impl Widget for Text {
     fn render(&self, rect: &Rect) {
         mv(rect.y as i32, rect.x as i32);
         // TODO: Text does not support wrapping around
-        addstr(self.text.get(..rect.w.floor() as usize).unwrap_or(&self.text));
+        let s = self.text.get(..rect.w.floor() as usize).unwrap_or(&self.text);
+        let n = s.len();
+        let free_space = rect.w - n as f32;
+        match self.halign {
+            HAlign::Left => {
+                addstr(s);
+            }
+            HAlign::Centre => {
+                let padding = (free_space * 0.5).floor() as usize;
+                for _ in 0..padding {
+                    addstr(" ");
+                }
+                addstr(s);
+                for _ in 0..padding {
+                    addstr(" ");
+                }
+            }
+            HAlign::Right => {
+                let padding = free_space.floor() as usize;
+                for _ in 0..padding {
+                    addstr(" ");
+                }
+                addstr(s);
+            }
+        }
     }
 
-    fn handle_event(&mut self, _event: &Event) {
+    fn handle_event(&mut self, event: &Event) {
+        match event {
+            Event::Realign(halign) => self.halign = *halign,
+        }
     }
 }
 
@@ -85,8 +124,8 @@ fn screen_rect() -> Rect {
     Rect {x: 0.0, y: 0.0, w: w as f32, h: h as f32}
 }
 
-fn text(text: &str) -> Box<dyn Widget> {
-    Box::new(Text { text: text.to_string() })
+fn text(text: &str, halign: HAlign) -> Box<dyn Widget> {
+    Box::new(Text { text: text.to_string(), halign })
 }
 
 fn hbox(widgets: Vec<Box<dyn Widget>>) -> Box<dyn Widget> {
@@ -108,19 +147,26 @@ fn main() {
         }
     }));
 
-    let ui = vbox(vec![
-        hbox(vec![ text("hello"), text("hello"), text("hello") ]),
-        hbox(vec![ text("world"), text("world"), text("world") ]),
-        hbox(vec![ text("foo"), text("foo"), text("foo") ]),
-        hbox(vec![ text("bar"), text("bar"), text("bar") ]),
+    // TODO: extract this to examples
+    let halign = HAlign::Right;
+    let mut ui = vbox(vec![
+        hbox(vec![ text("hello", halign), text("hello", halign), text("hello", halign) ]),
+        hbox(vec![ text("world", halign), text("world", halign), text("world", halign) ]),
+        hbox(vec![ text("foo", halign), text("foo", halign), text("foo", halign) ]),
+        hbox(vec![ text("bar", halign), text("bar", halign), text("bar", halign) ]),
     ]);
 
     loop {
         erase();
         ui.render(&screen_rect());
         let key = getch();
-        if key == 'q' as i32 {
-            break;
+
+        match key as u8 as char {
+            'q' => break,
+            'z' => ui.handle_event(&Event::Realign(HAlign::Left)),
+            'x' => ui.handle_event(&Event::Realign(HAlign::Centre)),
+            'c' => ui.handle_event(&Event::Realign(HAlign::Right)),
+            _ => {}
         }
     }
 
