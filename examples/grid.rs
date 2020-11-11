6d10c076daa99480c3516e350c100fe4a47fc6d6
rexim <reximkut@gmail.com>
2020-11-11 12:53:57 +0700

    Simplify grid example

--- examples/grid.rs
+++ examples/grid.rs
@@ -1,78 +1,11 @@
 use rcui::*;
 
-struct MyText {
-    text: Text,
-}
-
-impl MyText {
-    fn new(text: &str) -> Self {
-        Self {
-            text: Text::new(text),
-        }
-    }
-
-    fn wrap(text: &str) -> Box<Self> {
-        Box::new(Self::new(text))
-    }
-}
-
-impl Widget for MyText {
-    fn render(&mut self, rect: &rcui::Rect) {
-        self.text.render(rect)
-    }
-
-    fn handle_event(&mut self, event: &rcui::Event) {
-        match event {
-            Event::KeyStroke(key) => match *key as u8 as char {
-                'q' => {
-                    self.text.halign = HAlign::Left;
-                    self.text.valign = VAlign::Top;
-                }
-
-                'w' => {
-                    self.text.halign = HAlign::Centre;
-                    self.text.valign = VAlign::Top;
-                }
-
-                'e' => {
-                    self.text.halign = HAlign::Right;
-                    self.text.valign = VAlign::Top;
-                }
-
-                'a' => {
-                    self.text.halign = HAlign::Left;
-                    self.text.valign = VAlign::Centre;
-                }
-
-                's' => {
-                    self.text.halign = HAlign::Centre;
-                    self.text.valign = VAlign::Centre;
-                }
-
-                'd' => {
-                    self.text.halign = HAlign::Right;
-                    self.text.valign = VAlign::Centre;
-                }
-
-                'z' => {
-                    self.text.halign = HAlign::Left;
-                    self.text.valign = VAlign::Bottom;
-                }
-
-                'x' => {
-                    self.text.halign = HAlign::Centre;
-                    self.text.valign = VAlign::Bottom;
-                }
-
-                'c' => {
-                    self.text.halign = HAlign::Right;
-                    self.text.valign = VAlign::Bottom;
-                }
-
-                _ => {}
-            },
-        }
-    }
+fn text_cell(s: &str) -> Box<Text> {
+    Box::new(Text {
+        text: s.to_string(),
+        halign: HAlign::Centre,
+        valign: VAlign::Centre,
+    })
 }
 
 fn main() {
@@ -80,7 +13,7 @@ fn main() {
         |origin, event| {
             match event {
                 Event::KeyStroke(key) => {
-                    if *key as u8 as char == 't' {
+                    if *key as u8 as char == 'q' {
                         rcui::quit();
                     }
                 }
@@ -89,25 +22,17 @@ fn main() {
         },
         VBox::new(vec![
             HBox::wrap(vec![
-                MyText::wrap("hello"),
-                MyText::wrap("hello"),
-                MyText::wrap("hello"),
+                text_cell("hello"),
+                text_cell("hello"),
+                text_cell("hello"),
             ]),
             HBox::wrap(vec![
-                MyText::wrap("world"),
-                MyText::wrap("world"),
-                MyText::wrap("world"),
+                text_cell("world"),
+                text_cell("world"),
+                text_cell("world"),
             ]),
-            HBox::wrap(vec![
-                MyText::wrap("foo"),
-                MyText::wrap("foo"),
-                MyText::wrap("foo"),
-            ]),
-            HBox::wrap(vec![
-                MyText::wrap("bar"),
-                MyText::wrap("bar"),
-                MyText::wrap("bar"),
-            ]),
+            HBox::wrap(vec![text_cell("foo"), text_cell("foo"), text_cell("foo")]),
+            HBox::wrap(vec![text_cell("bar"), text_cell("bar"), text_cell("bar")]),
         ]),
     ));
 
