e7b77c49ef65a8525bccd28c504c1380ba120400
rexim <reximkut@gmail.com>
2020-12-01 20:36:26 +0700

    (#34) Introduce cursor mechanism to EditField

--- src/edit_field.rs
+++ src/edit_field.rs
@@ -2,40 +2,63 @@ use super::*;
 
 #[derive(Default)]
 pub struct EditField {
-    text: String,
+    text: Vec<char>,
     buffer: Vec<u8>,
-    // TODO(#34): EditField does not have any cursor functionality
+    cursor: usize,
 }
 
 impl EditField {
     pub fn new() -> Self {
         Self {
-            text: String::new(),
+            text: Vec::new(),
             buffer: Vec::new(),
+            cursor: 0,
         }
     }
 
     pub fn wrap() -> Box<Self> {
         Box::new(Self::new())
     }
+
+    pub fn insert_chars(&mut self, cs: &[char]) {
+        if self.cursor >= self.text.len() {
+            self.text.extend_from_slice(cs);
+            self.cursor += cs.len();
+        } else {
+            for c in cs.iter() {
+                self.text.insert(self.cursor, *c);
+                self.cursor += 1;
+            }
+        }
+    }
 }
 
 impl Widget for EditField {
-    fn render(&mut self, _context: &mut Rcui, rect: &Rect, _active: bool) {
+    fn render(&mut self, _context: &mut Rcui, rect: &Rect, active: bool) {
         let x = rect.x.floor() as i32;
         let y = rect.y.floor() as i32;
         mv(y, x);
         // TODO(#35): EditField does not wrap during the rendering
-        addstr(&self.text);
+        addstr(&self.text.iter().collect::<String>());
+        if active {
+            mv(y, x + self.cursor as i32);
+            attron(COLOR_PAIR(style::CURSOR_PAIR));
+            if self.cursor >= self.text.len() {
+                addstr(" ");
+            } else {
+                addstr(&self.text[self.cursor].to_string());
+            }
+            attroff(COLOR_PAIR(style::CURSOR_PAIR));
+        }
     }
 
     fn handle_event(&mut self, _context: &mut Rcui, event: &Event) {
         // TODO(#37): move the utf8 buffer mechanism to the main event loop
         if let Event::KeyStroke(key) = event {
             self.buffer.push(*key as u8);
-            match std::str::from_utf8(&self.buffer) {
+            match String::from_utf8(self.buffer.clone()) {
                 Ok(s) => {
-                    self.text.push_str(&s);
+                    self.insert_chars(&s.chars().collect::<Vec<_>>());
                     self.buffer.clear()
                 }
                 Err(_) => {
