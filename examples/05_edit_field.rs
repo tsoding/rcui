92e266b021da64c92a65834fe59f9ab7e66bbc5e
rexim <reximkut@gmail.com>
2020-12-09 19:52:11 +0700

    Add TODO(#50)

--- examples/05_edit_field.rs
+++ examples/05_edit_field.rs
@@ -10,7 +10,7 @@ fn main() {
                     KEY_RIGHT => field.right(),
                     KEY_DC => field.delete_front(),
                     KEY_BACKSPACE => field.delete_back(),
-                    // TODO: Replace KEY_F1 and KEY_F2 with Shift+Left and Shift+Right in 05_edit_field
+                    // TODO(#50): Replace KEY_F1 and KEY_F2 with Shift+Left and Shift+Right in 05_edit_field
                     KEY_F1 => field.select_left(),
                     KEY_F2 => field.select_right(),
                     _ => {
